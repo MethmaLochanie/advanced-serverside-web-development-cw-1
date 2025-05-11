13ea838da4d687ebb14d0bc765effcfee5f32d95
Methuu1119 <104524747+MethmaLochanie@users.noreply.github.com>
2025-05-11 13:25:29 +0530

    bug fix: create separate api file

--- client/src/api/blogPosts.js
+++ client/src/api/blogPosts.js
@@ -1,53 +1,39 @@
-import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+import api from './api';
 
 export const blogPostService = {
     // Get all blog posts
     getAllPosts: async () => {
-        const response = await axios.get(`${API_URL}/posts`);
+        const response = await api.get('/posts');
         return response.data;
     },
 
     // Get a single blog post
     getPost: async (id) => {
-        const response = await axios.get(`${API_URL}/posts/${id}`);
+        const response = await api.get(`/posts/${id}`);
         return response.data;
     },
 
     // Create a new blog post
     createPost: async (postData) => {
-        const response = await axios.post(`${API_URL}/posts`, postData, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        });
+        const response = await api.post('/posts', postData);
         return response.data;
     },
 
     // Update a blog post
     updatePost: async (id, postData) => {
-        const response = await axios.put(`${API_URL}/posts/${id}`, postData, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        });
+        const response = await api.put(`/posts/${id}`, postData);
         return response.data;
     },
 
     // Delete a blog post
     deletePost: async (id) => {
-        const response = await axios.delete(`${API_URL}/posts/${id}`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        });
+        const response = await api.delete(`/posts/${id}`);
         return response.data;
     },
 
     // Search blog posts by country
     searchByCountry: async (country, page = 1, limit = 10) => {
-        const response = await axios.get(`${API_URL}/posts/search/country`, {
+        const response = await api.get('/posts/search/country', {
             params: { country, page, limit }
         });
         return response.data;
@@ -55,7 +41,7 @@ export const blogPostService = {
 
     // Search blog posts by username
     searchByUsername: async (username, page = 1, limit = 10) => {
-        const response = await axios.get(`${API_URL}/posts/search/username`, {
+        const response = await api.get('/posts/search/username', {
             params: { username, page, limit }
         });
         return response.data;
--- client/src/api/followApi.js
+++ client/src/api/followApi.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+import api from './api';
 
 // Get auth token from localStorage
 const getAuthToken = () => {
@@ -10,55 +8,36 @@ const getAuthToken = () => {
 
 // Follow a user
 export const followUser = async (followingId) => {
-    const response = await axios.post(`${API_URL}/follow/follow`, {
+    const response = await api.post('/follow/follow', {
         followingId
-    }, {
-        headers: {
-            Authorization: getAuthToken()
-        }
     });
     return response.data;
 };
 
 // Unfollow a user
 export const unfollowUser = async (followingId) => {
-    const response = await axios.post(`${API_URL}/follow/unfollow`, {
+    const response = await api.post('/follow/unfollow', {
         followingId
-    }, {
-        headers: {
-            Authorization: getAuthToken()
-        }
     });
     return response.data;
 };
 
 // Get user's followers
 export const getFollowers = async (userId) => {
-    const response = await axios.get(`${API_URL}/follow/followers/${userId}`, {
-        headers: {
-            Authorization: getAuthToken()
-        }
-    });
+    const response = await api.get(`/follow/followers/${userId}`);
     return response.data;
 };
 
 // Get user's following
 export const getFollowing = async (userId) => {
-    const response = await axios.get(`${API_URL}/follow/following/${userId}`, {
-        headers: {
-            Authorization: getAuthToken()
-        }
-    });
+    const response = await api.get(`/follow/following/${userId}`);
     return response.data;
 };
 
 // Get followed users' posts
 export const getFollowedUsersPosts = async (userId, page = 1, limit = 10) => {
-    const response = await axios.get(`${API_URL}/follow/feed/${userId}`, {
-        params: { page, limit },
-        headers: {
-            Authorization: getAuthToken()
-        }
+    const response = await api.get(`/follow/feed/${userId}`, {
+        params: { page, limit }
     });
     return response.data;
 }; 
